36439e2199d6468222359e099932930af0a9f042
if-romario <romariojdosantos@gmail.com>
2025-05-25 23:17:54 -0300

    Add user API endpoint and implement JWT token expiration

--- functions.php
+++ functions.php
@@ -6,3 +6,10 @@ require_once($template_diretorio . "/custom-post-type/produto.php");
 require_once($template_diretorio . '/custom-post-type/transacao.php');
 
 require_once($template_diretorio . '/enpoints/usuario-post.php');
+require_once($template_diretorio . '/enpoints/usuario-get.php');
+
+function expire_token() {
+	return time() + (60 * 60 * 24);
+}
+
+add_action('jwt_auth_expire', 'expire_token');
